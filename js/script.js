549afdcc97cd35b8d8360734afd6435fe2355be0
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-12-03 14:26:08 +0530

    fix: get string based on boolean read

--- js/script.js
+++ js/script.js
@@ -2,11 +2,10 @@ function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.read = read ? "already read": "not read yet";
   this.id = crypto.randomUUID();
   
   this.info = function() {
-    read = read ? "already read": "not read yet";
     const text = `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 
     return text;
