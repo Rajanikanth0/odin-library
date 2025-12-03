98dca37ef16162e3ae118e84112d7ba6c373f187
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-12-03 20:08:26 +0530

    feat: add form into a dialog tag

--- js/script.js
+++ js/script.js
@@ -31,6 +31,13 @@ function addBookToPage() {
 
 }
 
+function newButtonClicked(e) {
+  e.preventDefault();
+
+  const dialog = document.querySelector("dialog");
+  dialog.showModal();
+}
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
 addBookToLibrary("Harry City", "P.S. Pray Silicon", 816, false);
 addBookToLibrary("Songs of Road", "W.H.O My Self", 190, true);
@@ -38,3 +45,7 @@ addBookToLibrary("Many Returns Of the Day", "Silver Liner S.S", 572, false);
 addBookToLibrary("Solar Panel Rays", "Dr. John Wonder Lee", 356, true);
 
 addBookToPage();
+
+const newButton = document.querySelector(".new-button");
+
+newButton.addEventListener("click", newButtonClicked);
